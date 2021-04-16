05088b05e7aac178318c266ea79f4a3a6ea29c1d
Hari Krishna <hari10599@gmail.com>
2021-04-16 23:57:58 +0530

    Intial Commit

---
+++ LoginToVM.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/expect
+
+#Increase timeout incase your VM is slow.
+set timeout 3
+set action [lindex $argv 0];
+set host "enter host"
+set ip "enter ip"
+set password "enter password"
+
+#ssh into VM
+spawn ssh $host@$ip
+expect "password"
+send "$password\r"
+expect "$"
+send "sudo -i\r"
+expect "password"
+send "$password\r"
+
+#Take action based on the argument
+if { $action == "docker" } {
+    expect "$"
+    #This will only work if you have a single container running, if you have multiple containers remove "\$(docker ps -q)" and paste your container id
+    send "docker exec -it \$(docker ps -q) bash\r"
+} elseif { $action == "jetty"} {
+    expect "$"
+    send "cd /var/lib/docker/volumes/shared/_data/jetty/jetty-distribution-9.4.38.v20210224/\r"
+    expect "$"
+    send "kill \$(lsof -t -i:12345)\r"
+    expect "$"
+    send "java -jar start.jar\r"
+}
+
+interact
+
